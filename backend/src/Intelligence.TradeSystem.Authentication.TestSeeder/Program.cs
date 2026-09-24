549b4c92edd4f3a3b9bbde1d42d1627f3bf7689b
v.bondarev <bondarev.vladimir.87@gmail.com>
2026-09-25 00:57:40 +0300

    #152: Откатил несогласованные изменения

--- backend/src/Intelligence.TradeSystem.Authentication.TestSeeder/Program.cs
+++ backend/src/Intelligence.TradeSystem.Authentication.TestSeeder/Program.cs
@@ -2,9 +2,9 @@ namespace Intelligence.TradeSystem.Authentication.TestSeeder;
 
 public static class Program
 {
-    public static async Task Main(string[] args)
+    public static async Task Main()
     {
-        var builder = Host.CreateApplicationBuilder(args);
+        var builder = Host.CreateApplicationBuilder();
         builder.Services.AddAuthenticationTestSeeding(builder.Configuration);
 
         using var host = builder.Build();
